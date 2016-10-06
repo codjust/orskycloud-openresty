4c95b52754ee21bd9f83edb9fc02f7a553131128
huchangwei <huchangwei@360.cn>
2016-10-07 00:47:24 +0800

    修正conf，写了点逻辑

--- nginx/lua/api/uploadData.lua
+++ nginx/lua/api/uploadData.lua
@@ -1,2 +1,20 @@
---涓婁紶鏁版嵁
--- 瑙ｆ瀽POST璇锋眰锛屾牴鎹姹傛暟鎹redis杩涜鎿嶄綔
+--上传数据
+-- 解析POST请求，根据请求数据对redis进行操作
+--请求样例：curl -i '127.0.0.1/api/uploadData.json?uid=1&did=3'
+local args = ngx.req.get_uri_args()
+
+-- if  args.uid == nil or args.did == nil then 
+-- 	ngx.log(ngx.WARN,"request args uid is nil or did is nil")
+-- 	ngx.exit(ngx.HTTP_BAD_REQUEST)
+-- end
+
+local uid = args.uid 
+local did = args.did
+
+if not uid then
+	ngx.log(ngx.WARN,"bad request args uid P:",uid)
+	ngx.exit(ngx.HTTP_BAD_REQUEST)
+end
+
+ngx.log(ngx.WARN,"right request args uid :",uid)
+ngx.say("success")
