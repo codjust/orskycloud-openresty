bfc5ecffcd47d5b9f981b179f95f6f595fb6e46f
huchangwei <huchangwei@360.cn>
2016-10-11 17:33:53 +0800

    add redis package

--- nginx/lua/api/uploadData.lua
+++ nginx/lua/api/uploadData.lua
@@ -1,13 +1,39 @@
-local comm=require("lua.comm.common") 
 
+local comm  = require("lua.comm.common") 
+local redis = require("lua.db_redis.db_base")
+local red   = redis:new()
 --上传数据
 -- 解析POST请求，根据请求数据对redis进行操作
 --请求样例：curl -i '127.0.0.1/api/uploadData.json?uid=1&did=3'
+
+-- 用户上传数据格式：
+-- URL：  127.0.0.1/api/uploadData.json?uid=""&&did=
+-- 数据：
+--   [
+--         {
+--             "sensor": "weight",
+--             "value": 78    
+--         },
+--         {
+--             "sensor": "heart",
+--             "value": 78
+--         }
+--     ]
+-- }
+
+-- 返回信息：
+-- {
+--    "Successful": false,
+--    "Message": "Invalid device ID"
+-- }
+
+
 local args = ngx.req.get_uri_args()
 
 local uid = args.uid 
 local did = args.did
 
+--uid 参数的合法性校验在access阶段处理，接口不另外解决
 if not uid or not did then
 	ngx.log(ngx.WARN,"bad request args uid,did:",uid,did)
 	ngx.exit(ngx.HTTP_BAD_REQUEST)
@@ -25,6 +51,22 @@ if  comm.check_args(post_args,{}) == false then
 	ngx.log(ngx.WARN,"error request body,probably is not a table")
 	ngx.exit(ngx.HTTP_BAD_REQUEST)
 end
+
+local response = {}
+
+local res,err = red:hget("uid:"..uid,"did:"..did)
+if err then
+	ngx.log(ngx.WARN,"redis hget uid:"..uid.."error")
+	ngx.exit(ngx.HTTP_SERVICE_UNAVAILABLE)
+end
+
+if res == nil then
+   response.Successful = false
+   response.Message = "device id not exist or user not exist"
+   ngx.say(comm.json_encode(response))
+end
+
+
 
 ngx.log(ngx.WARN,"right request args uid :",uid)
 ngx.say(body)
