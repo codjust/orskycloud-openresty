52efe26df66c0b0fc96f165fa05bf8878639bd00
huchangwei <huchangwei@360.cn>
2016-10-13 19:14:54 +0800

    finish template html

--- nginx/lua/template/html.lua
+++ nginx/lua/template/html.lua
@@ -1,5 +1,14 @@
 
 local template = require("resty.template")
 
+-- local view = template.new()
+-- ngx.log(ngx.WARN,"html....")
+-- template.render("view.html", {message = "hello world"})
+
 ngx.log(ngx.WARN,"html....")
-template.render("view.html", {message = "hello world"})
+-- local view = template.new "view.html"
+-- view.message = "Hello, World!"
+-- view:render()
+template.render("view.html", {message = "world"})
+
+ngx.log(ngx.WARN,"html....end")
