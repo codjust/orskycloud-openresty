7cfbe68013eb89df33aca4ad233209b1434d0d4f
huchangwei <1040385974@qq.com>
2016-12-20 16:53:53 +0800

    add new test case

--- nginx/main_server/test_base_lua/unit/getDeviceData.lua
+++ nginx/main_server/test_base_lua/unit/getDeviceData.lua
@@ -45,7 +45,7 @@ function tb:test_001_normal_return_data_not_select_time()
 end
 
 
-function tb:test_002_normal_return_data_select_time()
+function tb:test_002_normal_return_data_select_time_01()
 	local res, err = ngx.location.capture(self.uri .. self.uid .. "&did=" .. self.did .. "&StartTime=2015-10-20 14:50:30",
 		{method = ngx.HTTP_POST})
 
@@ -54,6 +54,32 @@ function tb:test_002_normal_return_data_select_time()
 	local data = common.json_decode(res.body)
 	local ret_data = data["1"]
 	local exp_data = {{sensor = "weight", value = 56, timestamp = "2016-10-20 14:50:30"}, {sensor = "high", value = 170, timestamp = "2016-11-22 14:50:30"}}
+
+	if common.is_table_equal(ret_data, exp_data) == false then
+		tb:log("ret_data:", common.json_encode(ret_data))
+		tb:log("exp_data:", common.json_encode(exp_data))
+		error("return data not equal self.data.")
+	end
+
+	local ret_msg     = data["Message"]
+	local ret_success = data["Successful"]
+	if ret_msg ~= "success" or ret_success ~= true then
+		tb:log("ret_msg:", ret_msg)
+		tb:log("ret_success:", ret_success)
+		error("ret_msg or ret_success failed.")
+	end
+end
+
+
+function tb:test_003_normal_return_data_select_time_02()
+	local res, err = ngx.location.capture(self.uri .. self.uid .. "&did=" .. self.did .. "&StartTime=2015-10-20 14:50:30&EndTime=2016-11-22 14:50:29",
+		{method = ngx.HTTP_POST})
+
+	assert(res.status == 200)
+
+	local data = common.json_decode(res.body)
+	local ret_data = data["1"]
+	local exp_data = {{sensor = "weight", value = 56, timestamp = "2016-10-20 14:50:30"}}
 
 	if common.is_table_equal(ret_data, exp_data) == false then
 		tb:log("ret_data:", common.json_encode(ret_data))
